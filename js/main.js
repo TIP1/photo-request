4c4baf1a8f5546a37204cfc0cdf2be386bc89b9e
IgEKirichenko <igekirichenko@mai.ru>
2020-11-16 23:31:26 +0300

    ready

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ var swiperHead = new Swiper('.header .swiper-container', {
       prevEl: '.header .swiper-button-prev',
     },
   });
+
+var swiperSecond = new Swiper('.second-slider .swiper-container', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.second-slider .swiper-button-next',
+    prevEl: '.second-slider .swiper-button-prev',
+  },
+});
+
+var swiperLast = new Swiper('.last-slider .swiper-container', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.last-slider .swiper-button-next',
+    prevEl: '.last-slider .swiper-button-prev',
+  },
+});
