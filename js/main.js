da01e8e8f535e0eb72d7e039b4ff2c4f2c8b07c9
TIP1 <tip219@bk.ru>
2020-12-09 15:54:31 +0200

    mobile adapt

--- js/main.js
+++ js/main.js
@@ -16,8 +16,18 @@ var swiperSecond = new Swiper('.second-slider .swiper-container', {
 
 var swiperLast = new Swiper('.last-slider .swiper-container', {
   spaceBetween: 10,
+  freeMode: true,
   navigation: {
     nextEl: '.last-slider .swiper-button-next',
     prevEl: '.last-slider .swiper-button-prev',
   },
 });
+
+$(window).on('load resize', function() {
+  if($(window).width() < 751) {
+    $('#shuldbechange').attr('src', './img/cards/11.png');
+  } 
+  if($(window).width() > 750) {
+    $('#shuldbechange').attr('src', './img/cards/4.png');
+  }
+})
